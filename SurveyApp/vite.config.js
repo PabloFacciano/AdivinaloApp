bb557acb9f21924e60fd173441bbb3d73eef9546
Pablo Facciano <pablo.facciano@globant.com>
2024-12-15 09:49:49 -0300

    add https

--- SurveyApp/vite.config.js
+++ SurveyApp/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from 'tailwindcss';
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    https: true
+  },
+  plugins: [
+    vue(),
+    mkcert()
+  ],
   css: {
     postcss: {
-      plugins: [tailwindcss],
+      plugins: [
+        tailwindcss
+      ],
     },
   },
 });
