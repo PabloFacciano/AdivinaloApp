51f9acc1fdb60a54fb7eca3d6a018dbe69b3cfd5
Pablo Facciano <pablo.facciano@globant.com>
2024-12-15 18:30:02 -0300

    prevent duplicated async calls for users

--- SurveyApp/src/stores/main.js
+++ SurveyApp/src/stores/main.js
@@ -6,6 +6,7 @@ export const useMainStore = defineStore('main', {
   state: () => ({
     darkMode: true,
     users: {},
+    loadingUser: {},
     currentUserId: 1234
   }),
   actions: {
@@ -13,8 +14,15 @@ export const useMainStore = defineStore('main', {
       if (_id in this.users){
         return this.users[_id];
       }
+      if (_id in this.loadingUser){
+        // in case of parallel calls for same user
+        return await this.loadingUser[_id];
+      }
 
-      this.users[_id] = await User.getFromDatabase(_id);
+      this.loadingUser[_id] = User.getFromDatabase(_id);
+      this.users[_id] = await this.loadingUser[_id];
+      delete this.loadingUser[_id];
+
       return this.users[_id];
     }
   },
