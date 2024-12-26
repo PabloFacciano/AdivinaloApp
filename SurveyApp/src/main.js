33104432b88978f15fd04a34d719f1eaee6d2011
Pablo Facciano <pablo.facciano@globant.com>
2024-12-26 12:43:55 -0300

    vue-rewards

--- SurveyApp/src/main.js
+++ SurveyApp/src/main.js
@@ -13,4 +13,8 @@ app.use(pinia);
 import router from './router/router.js'
 app.use(router);
 
+// https://github.com/HamadaFMahdi/vue-rewards
+import VueRewards from "vue-rewards";
+app.use(VueRewards);
+
 app.mount('#app');
